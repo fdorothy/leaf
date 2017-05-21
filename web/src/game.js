da9b3487c3131947dc1e4c3cc76b15d18d1c323c
Fredric Dorothy <fredric.dorothy@vipaar.com>
2017-05-21 13:38:28 -0500

    added some basic controls for placing arrows and directing the spiders

--- web/src/game.js
+++ web/src/game.js
@@ -2,6 +2,13 @@ var game = new Phaser.Game(16 * 16, 24 * 16, Phaser.CANVAS, 'phaser-example', { 
 
 var spiders;
 var obstacles;
+var arrowsLayer;
+var goals;
+var redTileId = 98;
+var rotation = 0;
+var cursors;
+var spider_speed = 80;
+var max_spiders = 25;
 
 function preload() {
     game.load.tilemap('level1', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
@@ -14,17 +21,18 @@ function create() {
     map = game.add.tilemap('level1');
     map.addTilesetImage('basictiles', 'gametiles');
     grassLayer = map.createLayer('grass');
+    grassLayer.resizeWorld();
     grassLayer2 = map.createLayer('grass2');
-
     obstacles = map.createLayer('obstacles');
     map.setCollisionBetween(1, 2000, true, 'obstacles');
-
-    grassLayer.resizeWorld();
-
+    goals = map.createLayer('goals');
+    map.setCollisionBetween(1, 2000, true, 'goals');
+    arrowsLayer = map.createLayer('arrows');
+    map.setCollisionBetween(1, 2000, true, 'arrows');
     spiders = game.add.group();
-
     game.time.events.loop(Phaser.Timer.SECOND * 0.5, create_spider, this);
-    create_spider();
+    game.input.onTap.add(onTap, this);
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function create_spider() {
@@ -33,14 +41,38 @@ function create_spider() {
         game.physics.arcade.enable(spider);
         spider.animations.add('run');
         spider.animations.play('run', 10, true);
-        spider.body.velocity.y = 60;
-        spider.dir = [0, 60];
-        console.log("ok, all created");
+        spider.body.velocity.y = spider_speed;
+        spider.dir = [0, spider_speed];
     }
 }
 
-function turnLeft(spider, obstacle) {
-    console.log("spider hit obstacle, turning left...");
+function hitArrow(spider, arrow) {
+    if (arrow.index > 0) {
+        switch (arrow.dir) {
+        case 0:
+            spider.body.velocity.x = 0;
+            spider.body.velocity.y = -spider_speed;
+            break;
+        case 90:
+            spider.body.velocity.x = spider_speed;
+            spider.body.velocity.y = 0;
+            break;
+        case 180:
+            spider.body.velocity.x = 0;
+            spider.body.velocity.y = spider_speed;
+            break;
+        case 270:
+            spider.body.velocity.x = -spider_speed;
+            spider.body.velocity.y = 0;
+            break;
+        }
+        spider.dir[0] = spider.body.velocity.x;
+        spider.dir[1] = spider.body.velocity.y;
+    }
+}
+
+function hitWall(spider, obstacle) {
+    console.log("hitting wall");
     spider.body.velocity.x = spider.dir[1];
     spider.body.velocity.y = -spider.dir[0];
     spider.dir[0] = spider.body.velocity.x;
@@ -48,6 +80,44 @@ function turnLeft(spider, obstacle) {
     return true;
 }
 
+function hitGoal(spider, goal) {
+    console.log('goal');
+    spiders.remove(spider);
+}
+
+function onTap(pointer, doubleTap) {
+    var x = obstacles.getTileX(game.input.activePointer.worldX);
+    var y = obstacles.getTileY(game.input.activePointer.worldY);
+    var obstacleTile = map.getTile(x, y, obstacles);
+    var goalTile = map.getTile(x, y, goals);
+    var arrowTile = map.getTile(x, y, arrowsLayer);
+    if (obstacleTile == null && goalTile == null && arrowTile == null) {
+        var tile = map.putTile(redTileId, x, y, arrowsLayer);
+        tile.rotation = game.math.degToRad(rotation);
+        tile.dir = rotation;
+    }
+}
+
 function update() {
-    game.physics.arcade.collide(spiders, obstacles, turnLeft);
+    game.physics.arcade.collide(spiders, arrowsLayer, null, hitArrow);
+    game.physics.arcade.collide(spiders, obstacles, hitWall);
+    game.physics.arcade.collide(spiders, goals, hitGoal);
+
+    if (cursors.left.isDown)
+    {
+        rotation = 270;
+    }
+    else if (cursors.right.isDown)
+    {
+        rotation = 90;
+    }
+
+    if (cursors.up.isDown)
+    {
+        rotation = 0;
+    }
+    else if (cursors.down.isDown)
+    {
+        rotation = 180;
+    }
 }
